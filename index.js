0337d6ddcee941d94410aa56713f87ba23c7b410
devsrinione <srinivasan.s@dsmsoft.net>
2024-02-01 13:08:44 +0530

    good one

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const app = express();
+
+const Router = require("./Route/Router");
+require("dotenv").config();
+
+app.use(express.json());
+app.listen(process.env.PORT, () => {
+  app.get("/", async (req, res) => {
+    res.status(200).send({ success: "Success" });
+  });
+  console.log("Server is running on port" + process.env.PORT);
+});
+app.use("/api", Router);
