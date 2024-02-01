1e0eb348a59ebeed054c45355ce18baa15d61f64
devsrinione <140168220+devsrinione@users.noreply.github.com>
2024-02-01 17:30:46 +0530

    Update reppref

--- Route/Router.js
+++ Route/Router.js
@@ -539,5 +539,39 @@ router.post("/filterReportData", async (req, res) => {
         }
         reqData();
       });
-
+   router.post("/reportPrefData", async (req, res) => {
+        const { filterdata, requst } = req.body;
+      
+        const url = process.env.BASE_URL + requst
+        console.log(url);
+        async function reqData() {
+          await axios
+            .post(url, filterdata, {
+              headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+              },
+              responseType: "stream",
+              httpsAgent: new Agent({ rejectUnauthorized: false }),
+            })
+            .then(function (response) {
+              let data = "";
+              response.data.on("data", (chunk) => {
+                data += chunk.toString();
+              });
+              response.data.on("end", () => {
+                const stringResponse = JSON.stringify(data);
+                if (stringResponse.match("html") || data === undefined) {
+                  res.status(400).json({
+                    message: "server response is unknown",
+                  });
+                } else {
+                  const jsonData = JSON.parse(data);
+                  res.status(200).send(jsonData);
+                }
+              });
+            });
+        }
+        reqData();
+      });
 module.exports = router;
